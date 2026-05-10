249660a82eb633505acab057220d8d2542afe025
Sanaya James <sanaya.j.james@gmail.com>
2026-05-09 21:10:45 -0700

    completed lesson

--- server/models.py
+++ server/models.py
@@ -20,3 +20,35 @@ class User(db.Model):
 class UserSchema(Schema):
     id = fields.Int()
     username = fields.String()
+
+class Login(Resource):
+
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+        
+        if user:
+            session['user_id'] = user.id
+            return UserSchema().dump(user)
+        else:
+            return {'message': 'Invalid login'}, 401
+        
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return UserSchema().dump(user)
+        else:
+            return {'message': '401: Not Authorized'}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
